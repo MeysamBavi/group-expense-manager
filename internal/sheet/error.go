41b2df3444e6ca2759d95562fd4c457be36fb195
Meysam Bavi <meybsky@gmail.com>
2023-03-11 03:09:27 +0330

    Remove CellError struct

--- internal/sheet/error.go
+++ internal/sheet/error.go
@@ -2,20 +2,10 @@ package sheet
 
 import "fmt"
 
-type CellError struct {
-	cellName  string
-	sheetName string
-	err       error
-}
-
-func (c CellError) Error() string {
-	return fmt.Sprintf("%v: in %q at %q", c.err, c.sheetName, c.cellName)
-}
-
 func CellErrorOf(err error, sheetName, cellName string) error {
 	if err == nil {
 		return nil
 	}
 
-	return CellError{err: err, sheetName: sheetName, cellName: cellName}
+	return fmt.Errorf("%w: in %q at %q", err, sheetName, cellName)
 }
