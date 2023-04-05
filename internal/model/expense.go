d3887fdbf540b7c875722a8a80be89b08e5d03e5
Meysam Bavi <meybsky@gmail.com>
2023-04-05 05:12:33 +0330

    Refactor weight parse logic, fix empty string case (#14)

--- internal/model/expense.go
+++ internal/model/expense.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"strconv"
+	"strings"
+)
+
 type Share struct {
 	MemberName  string
 	ShareWeight int
@@ -19,4 +24,24 @@ func (e *Expense) SumOfWeights() int {
 		sum += share.ShareWeight
 	}
 	return sum
+}
+
+func ParseShareWeight(weightStr string) (int, error) {
+	weightStr = strings.TrimSpace(weightStr)
+
+	if weightStr == "" {
+		return 0, nil
+	}
+
+	if b, err := strconv.ParseBool(weightStr); err == nil {
+		var weight int
+		if b {
+			weight = 1
+		} else {
+			weight = 0
+		}
+		return weight, nil
+	}
+
+	return strconv.Atoi(weightStr)
 }
